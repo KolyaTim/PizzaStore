18e919f6f883d3f161b3c3d54638d710f4e06913
Timofey <novoselov_t@list.ru>
2023-04-26 15:51:49 +0700

    PizzaStore

---
+++ Pizza/PizzaStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pizza
+{
+    abstract class PizzaStore
+    {
+        public virtual Pizza orderPizza(string type)
+        {
+            Pizza pizza = createPizza(type);
+
+            pizza.prepare();
+            pizza.bake();
+            pizza.cut();
+            pizza.box();
+            return pizza;
+        }
+
+
+
+        protected abstract Pizza createPizza(string type);
+
+    }
+}
